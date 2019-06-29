deef64acfbd7e4390e7bb6876ad0815c642c052b
tarunChand28298 <44527962+tarunChand28298@users.noreply.github.com>
2019-06-29 19:31:26 +0545

    Update Display.h

--- Graphics/Source/Display.h
+++ Graphics/Source/Display.h
@@ -2,6 +2,7 @@
 #include "Input.h"
 #include <Windows.h>
 #include <utility>
+#include <chrono>
 
 enum DrawMode {
 	Fill,
@@ -19,9 +20,10 @@ public:
 	void DestroyDisplay();
 	static LRESULT CALLBACK MainWindowProcedure(HWND windowHandle, UINT message, WPARAM wparam, LPARAM lparam);
 
-	int width = 1000;
-	int height = 600;
+	int width = 700;
+	int height = 700;
 	bool running = false;
+	float deltaTime = 0;
 
 	HWND mainWindowHandle = nullptr;
 	int* pixelBuffer = nullptr;
@@ -30,12 +32,13 @@ public:
 
 	BITMAPINFO pixelbufferInfo = {};
 	WNDCLASS windowClassInfo = {};
+	std::chrono::steady_clock::time_point previousFrameTime;
 
 	void PutPixel(int x, int y, unsigned char r, unsigned char g, unsigned char b, unsigned char p);
 	void PutPixel(int x, int y, int colour);
 
-	void DrawLine(int x1, int y1, int x2, int y2, int colour);										//Directly picked up from OLC pixel game engine.
-	void DrawCircle(int x, int y, int radius, int colour, DrawMode mode);							//Directly picked up from OLC pixel game engine.
+	void DrawLine(int x1, int y1, int x2, int y2, int colour);					//Directly picked up from OLC pixel game engine.
+	void DrawCircle(int x, int y, int radius, int colour, DrawMode mode);				//Directly picked up from OLC pixel game engine.
 	void DrawRectangle(int x, int y, int width, int height, int colour, DrawMode mode);
 	void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3, int colour, DrawMode mode);	//Directly picked up from OLC pixel game engine.
 
